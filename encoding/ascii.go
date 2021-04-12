551f1d9d5896797b22a3fdefdf2bb80ca9c690f6
Suresh Venkatraman <suresh@wise.us>
2021-04-12 11:14:30 -0700

    Adding ascii encoder variants to enforce alpha numeric values

--- encoding/ascii.go
+++ encoding/ascii.go
@@ -2,15 +2,52 @@ package encoding
 
 import "fmt"
 
-var ASCII Encoder = &asciiEncoder{}
+type encType int
 
-type asciiEncoder struct{}
+const (
+	asciiTypeDefault encType = iota
+	asciiTypeAlpha
+	asciiTypeNumeric
+	asciiTypeAlphaNumeric
+)
+
+var (
+	ASCII        = &asciiEncoder{asciiTypeDefault}
+	Alpha        = &asciiEncoder{asciiTypeAlpha}
+	Numeric      = &asciiEncoder{asciiTypeNumeric}
+	AlphaNumeric = &asciiEncoder{asciiTypeAlphaNumeric}
+)
+
+type asciiEncoder struct {
+	t encType
+}
 
 func (e asciiEncoder) Encode(data []byte) ([]byte, error) {
 	out := []byte{}
 	for _, r := range data {
-		if r > 127 {
-			return nil, fmt.Errorf("invalid ASCII char: '%s'", string(r))
+		switch e.t {
+		default:
+			if r > 127 {
+				return nil, fmt.Errorf("invalid ASCII char: '%s'", string(r))
+			}
+		case asciiTypeAlpha:
+			switch {
+			case r >= 65 && r <= 90, r >= 97 && r <= 122:
+				break
+			default:
+				return nil, fmt.Errorf("invalid ASCII alpha char: '%s'", string(r))
+			}
+		case asciiTypeNumeric:
+			if r < 48 || r > 57 {
+				return nil, fmt.Errorf("invalid ASCII numeric char: '%s'", string(r))
+			}
+		case asciiTypeAlphaNumeric:
+			switch {
+			case r >= 48 && r <= 57, r >= 65 && r <= 90, r >= 97 && r <= 122:
+				break
+			default:
+				return nil, fmt.Errorf("invalid ASCII alphanumeric char: '%s'", string(r))
+			}
 		}
 		out = append(out, r)
 	}
@@ -21,8 +58,29 @@ func (e asciiEncoder) Encode(data []byte) ([]byte, error) {
 func (e asciiEncoder) Decode(data []byte, _ int) ([]byte, error) {
 	out := []byte{}
 	for _, r := range data {
-		if r > 127 {
-			return nil, fmt.Errorf("invalid ASCII char: '%s'", string(r))
+		switch e.t {
+		default:
+			if r > 127 {
+				return nil, fmt.Errorf("invalid ASCII char: '%s'", string(r))
+			}
+		case asciiTypeAlpha:
+			switch {
+			case r >= 65 && r <= 90, r >= 97 && r <= 122:
+				break
+			default:
+				return nil, fmt.Errorf("invalid ASCII alpha char: '%s'", string(r))
+			}
+		case asciiTypeNumeric:
+			if r < 48 || r > 57 {
+				return nil, fmt.Errorf("invalid ASCII numeric char: '%s'", string(r))
+			}
+		case asciiTypeAlphaNumeric:
+			switch {
+			case r >= 48 && r <= 57, r >= 65 && r <= 90, r >= 97 && r <= 122:
+				break
+			default:
+				return nil, fmt.Errorf("invalid ASCII alphanumeric char: '%s'", string(r))
+			}
 		}
 		out = append(out, r)
 	}
